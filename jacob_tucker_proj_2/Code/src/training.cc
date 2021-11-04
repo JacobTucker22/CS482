fac3a84127cf8d3e2037073dc81a1eb1ab1de245
Jacob Tucker <jacobtucker@nevada.unr.edu>
2021-11-04 15:21:46 -0700

    Umaps update and total words counted

--- jacob_tucker_proj_2/Code/src/training.cc
+++ jacob_tucker_proj_2/Code/src/training.cc
@@ -23,6 +23,10 @@ using namespace std;
 
 
 int main(int argc, char *argv[]) {
+    if(argc != 7) {
+        cout << "Correct Usage: ./training -i <input file.csv> -os <spam output file.csv> -oh <ham output file.csv>\n"
+                "Order of arguments does not matter as long as correct flags are in front of file paths\n";
+    }
 
     //read write file paths
     string inputFilePath;
@@ -30,6 +34,7 @@ int main(int argc, char *argv[]) {
     string outputHamPath;
 
     //Count of total number of words in each class
+    int* totalWordsPtr;
     int totalHamWords = 0;
     int totalSpamWords = 0;
 
@@ -53,6 +58,8 @@ int main(int argc, char *argv[]) {
     }
 
     //Two unordered maps to store strings as keys and ints values representing frequency of word found
+    //Ptr points to which umap is being used at each line
+    unordered_map<string, int>* uMapPtr = nullptr;
     unordered_map<string, int> hamMap;
     unordered_map<string, int> spamMap;
 
@@ -73,14 +80,25 @@ int main(int argc, char *argv[]) {
     //Else add word to umap with initial value = 1
     while(getline(iFile, line)) {
         stringstream ssLine(line);
+
         //Holds value for type classification
         string type;
+        //Get type from first column and set umap ptr
+        getline(ssLine, type, ',');
+        if(type == "ham") {
+            uMapPtr = &hamMap;
+            totalWordsPtr = &totalHamWords;
+        }
+        else if (type == "spam") {
+            uMapPtr = &spamMap;
+            totalWordsPtr = &totalSpamWords;
+        }
+        else {
+            cout << "Unrecognized class type. Please use ham or spam\n";
+        }
+
         //holds the string of word data
         string data;
-
-        //Get type from first column
-        getline(ssLine, type, ',');
-
         //Get data from second column
         getline(ssLine, data);
 
@@ -88,13 +106,34 @@ int main(int argc, char *argv[]) {
         data.erase(remove_if(data.begin(), data.end(), ::ispunct), data.end());
         transform(data.begin(), data.end(), data.begin(), ::tolower);
 
+        //make stream of clean data for parsing
+        //Consider removing single character words (may not be helpful for classification)
+        stringstream ssData(data);
+        string word;
+        while(ssData >> word) {
+            if(ssData.peek() == EOF) {
+                break;
+            }
+            //increment total words for ham or spam
+            *totalWordsPtr = *totalWordsPtr + 1;
+            //check for value, increment count if it exists, or add with value = 1
+            auto it = uMapPtr->find(word);
+            if (it != uMapPtr->end()) {
+                it->second++;
+            }
+            else {
+                uMapPtr->insert(make_pair(word, 1));
+            }
+        }
+
+
+
 
 
     }
 
     //Close input file
     iFile.close();
-
 
 
     return 0;
