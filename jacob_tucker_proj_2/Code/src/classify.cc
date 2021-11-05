1c5d7a6265a4d1ba0bc48f1211432004757f4b1b
Jacob Tucker <jacobtucker@nevada.unr.edu>
2021-11-04 17:28:22 -0700

    Read in arguments

--- jacob_tucker_proj_2/Code/src/classify.cc
+++ jacob_tucker_proj_2/Code/src/classify.cc
@@ -14,9 +14,38 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
     if(argc != 9) {
-        cout << "Correct Usage: ./classify -i <input file.csv> -is <spam input file.csv> -ih <ham input file.csv> -o <class output file.csv>\n"
+        cout << "Correct Usage: ./classify -i <data to test.csv> -is <spam output file from training.csv> "
+                "-ih <ham output file from training.csv> -o <output file path.csv>\n"
                 "Order of arguments does not matter as long as correct flags are in front of file paths\n";
         return 1;
+    }
+
+    //prepended to file paths so it writes in main folder instead of build
+    const string prePath = "../";
+
+    //read write file paths
+    string inputDataPath;
+    string inputSpamPath;
+    string inputHamPath;
+    string outputPath;
+
+    //Store each argument after flag in appropriate string.
+    //Assumes working directory is build folder
+    //Read and writes to parent directory
+    for(int i = 0; i < argc; i++) {
+        string temp = argv[i];
+        if(strcmp( argv[i], "-i") == 0) {
+            inputDataPath = prePath + argv[i + 1];
+        }
+        else if(strcmp( argv[i], "-is") == 0) {
+            inputSpamPath = prePath + argv[i + 1];
+        }
+        else if(strcmp( argv[i], "-ih") == 0) {
+            inputHamPath = prePath + argv[i + 1];
+        }
+        else if(strcmp( argv[i], "-o") == 0) {
+            outputPath = prePath + argv[i + 1];
+        }
     }
 
 
