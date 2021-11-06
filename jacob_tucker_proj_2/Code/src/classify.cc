6e4c6d4173db0c3f724182a6719db18ac3598074
Jacob Tucker <jacobtucker@nevada.unr.edu>
2021-11-06 16:51:17 -0700

    classify working correctly.

--- jacob_tucker_proj_2/Code/src/classify.cc
+++ jacob_tucker_proj_2/Code/src/classify.cc
@@ -9,11 +9,38 @@
 #include <fstream>
 #include <sstream>
 #include <algorithm>
+#include <iomanip>
 
 using namespace std;
 
+//Function for reading the input files.
+//Takes the ifstream for the file to be read, the unordered map to populate with the data,
+//and a total words int to store the total number of words
+void readFile(ifstream &iFile, unordered_map<string, int> &uMap, double &totalWords) {
+    string line;
+    //get total word count
+    getline(iFile, line);
+    totalWords = stoi(line);
+
+    while (getline(iFile, line)) {
+        stringstream ssLine(line);
+        string word;
+        string tempFreq;
+        int freq;
+
+        //Parse line into word and frequency value
+        getline(ssLine, word, ',');
+        getline(ssLine, tempFreq);
+        freq = stoi(tempFreq);
+
+        //Insert word/freq pair into ham umap
+        uMap.insert(make_pair(word, freq));
+
+    }
+}
+
 int main(int argc, char *argv[]) {
-    if(argc != 9) {
+    if (argc != 9) {
         cout << "Correct Usage: ./classify -i <data to test.csv> -is <spam output file from training.csv> "
                 "-ih <ham output file from training.csv> -o <output file path.csv>\n"
                 "Order of arguments does not matter as long as correct flags are in front of file paths\n";
@@ -32,30 +59,155 @@ int main(int argc, char *argv[]) {
     //Store each argument after flag in appropriate string.
     //Assumes working directory is build folder
     //Read and writes to parent directory
-    for(int i = 0; i < argc; i++) {
+    for (int i = 0; i < argc; i++) {
         string temp = argv[i];
-        if(strcmp( argv[i], "-i") == 0) {
+        if (strcmp(argv[i], "-i") == 0) {
             inputDataPath = prePath + argv[i + 1];
-        }
-        else if(strcmp( argv[i], "-is") == 0) {
+        } else if (strcmp(argv[i], "-is") == 0) {
             inputSpamPath = prePath + argv[i + 1];
-        }
-        else if(strcmp( argv[i], "-ih") == 0) {
+        } else if (strcmp(argv[i], "-ih") == 0) {
             inputHamPath = prePath + argv[i + 1];
-        }
-        else if(strcmp( argv[i], "-o") == 0) {
+        } else if (strcmp(argv[i], "-o") == 0) {
             outputPath = prePath + argv[i + 1];
         }
     }
 
     //Read in each file. Save number of ham words and spam words
     //Save word and frequencies in umaps
-    //Add word counts to get total word count
-    //Calculate probabilty of class by class word count / total word count
-    //COnditional probability of each word = (freq of word in class + 1)/ (total words in class + Number of unique words among both classes)
-    //unique words should be equal to number of lines in count file - 1
-    //Probability of class = probability of class * each conditional probability of each word in message
-    //Calulate probability of class for each class, compare and choose the higher probability
+    unordered_map<string, int> hamMap;
+    unordered_map<string, int> spamMap;
+    double totalHamWords;
+    double totalSpamWords;
+
+    //Open ham input file for parsing
+    ifstream iFile(inputHamPath);
+    if (!iFile.is_open()) {
+        cout << "Error opening ham input file\n";
+        return 1;
+    }
+    readFile(iFile, hamMap, totalHamWords);
+
+    iFile.close();
+
+    //Open Spam input file for parsing
+    iFile.open(inputSpamPath);
+    if (!iFile.is_open()) {
+        cout << "Error opening spam input file\n";
+        return 1;
+    }
+    readFile(iFile, spamMap, totalSpamWords);
+
+    iFile.close();
+
+
+    //variables needed for algorithm
+    double probHamClass = totalHamWords / (totalHamWords + totalSpamWords);
+    double probSpamClass = totalSpamWords / (totalHamWords + totalSpamWords);
+    double spamSize = spamMap.size();
+    double hamSize = hamMap.size();
+    double totalUniqueWords = hamSize + spamSize;
+
+    //used to check the accuracy of algorithm
+    float totalMessages = 0;
+    float numCorrectClasses = 0;
+
+    //Open the file of messages to be classified
+    iFile.open(inputDataPath);
+    if (!iFile.is_open()) {
+        cout << "Error opening data to classify file\n";
+        return 1;
+    }
+
+    string line;
+    //ignore the first line headers
+    getline(iFile, line);
+
+    fstream oFile(outputPath, ios_base::out);
+    if (!oFile.is_open()) {
+        cout << "Error opening data to classify file\n";
+        return 1;
+    }
+    //write headers to output file
+    oFile << "v1,v2,,,\n";
+    oFile.close();
+
+    while (getline(iFile, line)) {
+        //keep track of total message
+        totalMessages++;
+        //parsing variables
+        stringstream ssLine(line);
+        string classType;
+        string data;
+        //store string before cleaning to write to output file
+        string originalMessage;
+
+        //store class type (for checking accuracy) and the message data
+        getline(ssLine, classType, ',');
+        getline(ssLine, data);
+        originalMessage = data;
+
+        //clean data. Remove punctuation and make lower case
+        data.erase(remove_if(data.begin(), data.end(), ::ispunct), data.end());
+        transform(data.begin(), data.end(), data.begin(), ::tolower);
+
+        //make stream of clean data for parsing
+        stringstream ssData(data);
+        string word;
+
+        //probability of class to be multiplied by conditional probability of each word
+        double probIsHam = probHamClass;
+        double probIsSpam = probSpamClass;
+
+        //Conditional probability of each word = (freq of word in class + 1)/ (total words in class + Number of unique words among both classes)
+        while (true) {
+            ssData >> word;
+            double condProb;
+            //calculate ham conditional probability
+            auto it = hamMap.find(word);
+            if (it != hamMap.end()) {
+                condProb = ((it->second) + 1) / (totalHamWords + totalUniqueWords);
+            } else {
+                condProb = 1 / (totalHamWords + totalUniqueWords);
+            }
+            probIsHam *= condProb;
+
+
+            //Calculate spam conditional probability
+            it = spamMap.find(word);
+            if (it != spamMap.end()) {
+                condProb = ((it->second) + 1) / (totalSpamWords + totalUniqueWords);
+            } else {
+                condProb = 1 / (totalSpamWords + totalUniqueWords);
+            }
+            probIsSpam *= condProb;
+
+
+            if (ssData.eof()) {
+                break;
+            }
+        }
+        //classify message based on which has the higher probability.
+        string messageClass = (probIsHam >= probIsSpam) ? "ham" : "spam";
+        oFile.open(outputPath, ios_base::app);
+        if(!oFile.is_open()) {
+            cout << "Error appending to output file\n";
+            return 1;
+        }
+        oFile << messageClass << ',' << originalMessage << "\n";
+        oFile.close();
+
+        if (messageClass == classType) {
+            numCorrectClasses++;
+        }
+        //cout << messageClass;
+
+
+    }
+    iFile.close();
+    //Display accuracy
+    double accuracy = (numCorrectClasses / totalMessages) * 100;
+    cout << "Classification accuracy: " << fixed << setprecision(2) << accuracy << '%' << endl;
+
 
 
     return 0;
--- jacob_tucker_proj_2/Code/src/training.cc
+++ jacob_tucker_proj_2/Code/src/training.cc
@@ -23,7 +23,7 @@ using namespace std;
 
 
 int main(int argc, char *argv[]) {
-    if(argc != 7) {
+    if (argc != 7) {
         cout << "Correct Usage: ./training -i <input file.csv> -os <spam output file.csv> -oh <ham output file.csv>\n"
                 "Order of arguments does not matter as long as correct flags are in front of file paths\n";
         return 1;
@@ -35,7 +35,7 @@ int main(int argc, char *argv[]) {
     string outputHamPath;
 
     //Count of total number of words in each class
-    int* totalWordsPtr;
+    int *totalWordsPtr;
     int totalHamWords = 0;
     int totalSpamWords = 0;
 
@@ -45,28 +45,26 @@ int main(int argc, char *argv[]) {
     //Store each argument after flag in appropriate string.
     //Assumes working directory is build folder
     //Read and writes to parent directory
-    for(int i = 0; i < argc; i++) {
+    for (int i = 0; i < argc; i++) {
         string temp = argv[i];
-        if(strcmp( argv[i], "-i") == 0) {
+        if (strcmp(argv[i], "-i") == 0) {
             inputFilePath = prePath + argv[i + 1];
-        }
-        else if(strcmp( argv[i], "-os") == 0) {
+        } else if (strcmp(argv[i], "-os") == 0) {
             outputSpamPath = prePath + argv[i + 1];
-        }
-        else if(strcmp( argv[i], "-oh") == 0) {
+        } else if (strcmp(argv[i], "-oh") == 0) {
             outputHamPath = prePath + argv[i + 1];
         }
     }
 
     //Two unordered maps to store strings as keys and ints values representing frequency of word found
     //Ptr points to which umap is being used at each line
-    unordered_map<string, int>* uMapPtr = nullptr;
+    unordered_map<string, int> *uMapPtr = nullptr;
     unordered_map<string, int> hamMap;
     unordered_map<string, int> spamMap;
 
     //Open input file for parsing
     ifstream iFile(inputFilePath);
-    if(!iFile.is_open()) {
+    if (!iFile.is_open()) {
         cout << "Error opening input file\n";
         return 1;
     }
@@ -80,22 +78,20 @@ int main(int argc, char *argv[]) {
     //Check each word in second value.
     //If word already in umap, increment value
     //Else add word to umap with initial value = 1
-    while(getline(iFile, line)) {
+    while (getline(iFile, line)) {
         stringstream ssLine(line);
 
         //Holds value for type classification
         string type;
         //Get type from first column and set umap ptr
         getline(ssLine, type, ',');
-        if(type == "ham") {
+        if (type == "ham") {
             uMapPtr = &hamMap;
             totalWordsPtr = &totalHamWords;
-        }
-        else if (type == "spam") {
+        } else if (type == "spam") {
             uMapPtr = &spamMap;
             totalWordsPtr = &totalSpamWords;
-        }
-        else {
+        } else {
             cout << "Unrecognized class type. Please use ham or spam\n";
         }
 
@@ -112,19 +108,19 @@ int main(int argc, char *argv[]) {
         //Consider removing single character words (may not be helpful for classification)
         stringstream ssData(data);
         string word;
-        while(ssData >> word) {
-            if(ssData.peek() == EOF) {
-                break;
-            }
+        while (true) {
+            ssData >> word;
             //increment total words for ham or spam
             *totalWordsPtr = *totalWordsPtr + 1;
             //check for value, increment count if it exists, or add with value = 1
             auto it = uMapPtr->find(word);
             if (it != uMapPtr->end()) {
                 it->second++;
-            }
-            else {
+            } else {
                 uMapPtr->insert(make_pair(word, 1));
+            }
+            if (ssData.eof()) {
+                break;
             }
         }
     }
@@ -133,13 +129,13 @@ int main(int argc, char *argv[]) {
 
     //Open Ham file and write values
     ofstream outHamFile(outputHamPath);
-    if(!outHamFile.is_open()) {
+    if (!outHamFile.is_open()) {
         cout << "Error opening output ham file\n";
         return 1;
     }
     outHamFile << totalHamWords << "\n";
 
-    for(const auto it : hamMap) {
+    for (const auto it: hamMap) {
         outHamFile << it.first << ','
                    << it.second << "\n";
     }
@@ -147,15 +143,15 @@ int main(int argc, char *argv[]) {
 
     //Open Spam file and write values
     ofstream outSpamFile(outputSpamPath);
-    if(!outSpamFile.is_open()) {
+    if (!outSpamFile.is_open()) {
         cout << "Error opening output spam file\n";
         return 1;
     }
     outSpamFile << totalSpamWords << "\n";
 
-    for(const auto it : spamMap) {
+    for (const auto it: spamMap) {
         outSpamFile << it.first << ','
-                   << it.second << "\n";
+                    << it.second << "\n";
     }
     outSpamFile.close();
 
